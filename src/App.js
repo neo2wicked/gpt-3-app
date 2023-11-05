6a83eab92e8c8fcf4f966dbd47da2bd9c2adcadc
neo2wicked <capricorn@Live.com>
2023-11-04 21:51:09 -0500

    updated initial folders and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { Footer, Blog, Possibility, Features, WhatGPT4, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
   return (
-    <div>
-    <h1>GPT-4</h1>
+    <div classname="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+
+    </div>
+    <Brand />
+    <WhatGPT4 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
     </div>
   )
 }
